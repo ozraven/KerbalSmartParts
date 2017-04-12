c2fecd25541fb181d5ddb206ee3d84bfab5f7774
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-04-12 17:18:09 -0400

    Fixed log spam caused by the conditional directives being commented out in Log.cs by PR submitted by @rakkhin: 	[ConditionalAttribute("DEBUG")] Added back disabled code in RadioControl.cs, for historical documentation purposes

--- Source/RadioControl.cs
+++ Source/RadioControl.cs
@@ -28,6 +28,83 @@ namespace Lib
         [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Channel"), UI_FloatRange(minValue = 1f, maxValue = 20f, stepIncrement = 1f)]
         public float channel = 1;
 
+
+#if false
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Group"),
+            UI_ChooseOption(
+            options = new String[] {
+                 "0",
+                 "1",
+                 "2",
+                 "3",
+                 "4",
+                 "5",
+                 "6",
+                 "7",
+                 "8",
+                 "9",
+                 "10",
+                 "11",
+                 "12",
+                 "13",
+                 "14",
+                 "15",
+                 "16"
+            },
+            display = new String[] {
+                 "Stage",
+                 "AG1",
+                 "AG2",
+                 "AG3",
+                 "AG4",
+                 "AG5",
+                 "AG6",
+                 "AG7",
+                 "AG8",
+                 "AG9",
+                 "AG10",
+                 "Lights",
+                 "RCS",
+                 "SAS",
+                 "Brakes",
+                 "Abort",
+                 "Gear"
+            }
+        )]
+        public string group = "0";
+
+        //AGXGroup shows if AGX installed and hides Group above		
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Group"),
+            UI_ChooseOption(
+            options = new String[] {
+                 "0",
+                 "1",
+                 "11",
+                 "12",
+                 "13",
+                 "14",
+                 "15",
+                 "16"
+            },
+            display = new String[] {
+                 "Stage",
+                 "Action Group:",
+                 "Lights",
+                 "RCS",
+                 "SAS",
+                 "Brakes",
+                 "Abort",
+                 "Gear"
+            }
+        )]
+        public string agxGroupType = "0";
+
+        // AGX Action groups, use own slider if selected, only show this field if AGXGroup above is 1		
+        [KSPField(isPersistant = true, guiActiveEditor = false, guiActive = false, guiName = "Group:", guiFormat = "N0"),
+            UI_FloatEdit(scene = UI_Scene.All, minValue = 1f, maxValue = 250f, incrementLarge = 75f, incrementSmall = 25f, incrementSlide = 1f)]
+        public float agxGroupNum = 1;
+#endif
+
         double fireTime = 0;
         double lightOnTime = 2;
         private string groupLastUpdate = "0"; //AGX: What was our selected group last update frame? Top slider.
@@ -36,7 +113,8 @@ namespace Lib
         public string rcv_sound = "";
 
         [KSPAction("Transmit")]
-        public void transmit_AG(KSPActionParam param) {
+        public void transmit_AG(KSPActionParam param)
+        {
             if (AGXInterface.AGExtInstalled())
             {
                 transmitCommand(float.Parse(agxGroupType), agxGroupNum);
@@ -48,78 +126,93 @@ namespace Lib
         }
 
         [KSPAction("Transmit Stage")]
-        public void transmit_AG0(KSPActionParam param) {
+        public void transmit_AG0(KSPActionParam param)
+        {
             transmitCommand(0);
         }
 
         [KSPAction("Transmit AG1")]
-        public void transmit_AG1(KSPActionParam param) {
+        public void transmit_AG1(KSPActionParam param)
+        {
             transmitCommand(1);
         }
 
         [KSPAction("Transmit AG2")]
-        public void transmit_AG2(KSPActionParam param) {
+        public void transmit_AG2(KSPActionParam param)
+        {
             transmitCommand(2);
         }
 
         [KSPAction("Transmit AG3")]
-        public void transmit_AG3(KSPActionParam param) {
+        public void transmit_AG3(KSPActionParam param)
+        {
             transmitCommand(3);
         }
 
         [KSPAction("Transmit AG4")]
-        public void transmit_AG4(KSPActionParam param) {
+        public void transmit_AG4(KSPActionParam param)
+        {
             transmitCommand(4);
         }
 
         [KSPAction("Transmit AG5")]
-        public void transmit_AG5(KSPActionParam param) {
+        public void transmit_AG5(KSPActionParam param)
+        {
             transmitCommand(5);
         }
 
         [KSPAction("Transmit AG6")]
-        public void transmit_AG6(KSPActionParam param) {
+        public void transmit_AG6(KSPActionParam param)
+        {
             transmitCommand(6);
         }
 
         [KSPAction("Transmit AG7")]
-        public void transmit_AG7(KSPActionParam param) {
+        public void transmit_AG7(KSPActionParam param)
+        {
             transmitCommand(7);
         }
 
         [KSPAction("Transmit AG8")]
-        public void transmit_AG8(KSPActionParam param) {
+        public void transmit_AG8(KSPActionParam param)
+        {
             transmitCommand(8);
         }
 
         [KSPAction("Transmit AG9")]
-        public void transmit_AG9(KSPActionParam param) {
+        public void transmit_AG9(KSPActionParam param)
+        {
             transmitCommand(9);
         }
 
         [KSPAction("Transmit AG10")]
-        public void transmit_AG10(KSPActionParam param) {
+        public void transmit_AG10(KSPActionParam param)
+        {
             transmitCommand(10);
         }
 
 
         [KSPAction("Transmit Light")]
-        public void transmit_Light(KSPActionParam param) {
+        public void transmit_Light(KSPActionParam param)
+        {
             transmitCommand(11);
         }
 
         [KSPAction("Transmit RCS")]
-        public void transmit_RCS(KSPActionParam param) {
+        public void transmit_RCS(KSPActionParam param)
+        {
             transmitCommand(12);
         }
 
         [KSPAction("Transmit SAS")]
-        public void transmit_SAS(KSPActionParam param) {
+        public void transmit_SAS(KSPActionParam param)
+        {
             transmitCommand(13);
         }
 
         [KSPAction("Transmit Brakes")]
-        public void transmit_Brakes(KSPActionParam param) {
+        public void transmit_Brakes(KSPActionParam param)
+        {
             transmitCommand(14);
         }
 
@@ -136,7 +229,8 @@ namespace Lib
         }
 
         [KSPEvent(guiName = "Transmit Command", guiActive = true)]
-        public void transmit_GUI() {
+        public void transmit_GUI()
+        {
             if (AGXInterface.AGExtInstalled())
             {
                 transmitCommand(float.Parse(agxGroupType), agxGroupNum);
@@ -152,22 +246,26 @@ namespace Lib
 
 
         [KSPAction("Transmit Throttle")]
-        public void transmit_ThrottleAG(KSPActionParam param) {
+        public void transmit_ThrottleAG(KSPActionParam param)
+        {
             transmitThrottle();
         }
 
         [KSPEvent(guiName = "Transmit Throttle", guiActive = true)]
-        public void transmit_Throttle() {
+        public void transmit_Throttle()
+        {
             transmitThrottle();
         }
 
         [KSPAction("Match Heading")]
-        public void transmit_pro_rotationAG(KSPActionParam param) {
+        public void transmit_pro_rotationAG(KSPActionParam param)
+        {
             transmitRotation(this.vessel.GetTransform().rotation, true);
         }
 
         [KSPEvent(guiName = "Match Heading", guiActive = true)]
-        public void transmit_pro_rotation() {
+        public void transmit_pro_rotation()
+        {
             transmitRotation(this.vessel.GetTransform().rotation, true);
         }
         /*
@@ -184,8 +282,10 @@ namespace Lib
         }
         */
 
-        public void transmitRotation(Quaternion rotation, bool playSound) {
-            foreach (var listener in Channel.radioListeners) {
+        public void transmitRotation(Quaternion rotation, bool playSound)
+        {
+            foreach (var listener in Channel.radioListeners)
+            {
                 if (listener != null && listener.vessel != null)
                     listener.receiveRotation(this, rotation, (int)channel, playSound);
             }
@@ -203,10 +303,12 @@ namespace Lib
         
         }*/
 
-        public void receiveRotation(RadioControl sender, Quaternion targetUp, int transmitChannel, bool playSound) {
+        public void receiveRotation(RadioControl sender, Quaternion targetUp, int transmitChannel, bool playSound)
+        {
 
 
-            if (this == sender || channel != transmitChannel || this.vessel == FlightGlobals.ActiveVessel) {
+            if (this == sender || channel != transmitChannel || this.vessel == FlightGlobals.ActiveVessel)
+            {
                 Log.Info("I am the active vessel or the sender or channels are not equal:" + channel + ", " + transmitChannel);
                 return;
             }
@@ -223,12 +325,14 @@ namespace Lib
         {
             transmitCommand(groupID, 1); //if agx is installed and groupID is 1, it will acivate agx so send 1 to activate correct group.
         }
-        
-        public void transmitCommand(float groupID, float agxGroupNumB) { //agxGroupNum only used when AGX installed, ignore otherwise
-            foreach (var listener in Channel.radioListeners) {
+
+        public void transmitCommand(float groupID, float agxGroupNumB)
+        { //agxGroupNum only used when AGX installed, ignore otherwise
+            foreach (var listener in Channel.radioListeners)
+            {
                 if (listener != null && listener.vessel != null)
-                { 
-                    if(AGXInterface.AGExtInstalled()) //AGX Edited
+                {
+                    if (AGXInterface.AGExtInstalled()) //AGX Edited
                     {
                         listener.receiveCommand(this, (int)groupID, (int)channel, (int)agxGroupNumB);
                     }
@@ -242,8 +346,10 @@ namespace Lib
             indicateSend();
         }
 
-        public void transmitThrottle() {
-            foreach (var listener in Channel.radioListeners) {
+        public void transmitThrottle()
+        {
+            foreach (var listener in Channel.radioListeners)
+            {
                 if (listener != null && listener.vessel != null)
                     listener.receiveThrottle(this, throttle, (int)channel);
             }
@@ -251,13 +357,15 @@ namespace Lib
 
         }
 
-        private void indicateSend() {
+        private void indicateSend()
+        {
             lightsOn();
             fireTime = Time.fixedTime;
             Log.Info("Fire Time:" + fireTime);
         }
 
-        private void indicateReceive(bool playSound) {
+        private void indicateReceive(bool playSound)
+        {
             lightsOn();
             fireTime = Time.fixedTime;
             if (playSound)
@@ -267,7 +375,8 @@ namespace Lib
 
         public void receiveCommand(RadioControl sender, int group, int transmitChannel, int agxGroup) //AGX Edited, agxGroup only used if AGX installed
         {
-            if (this == sender || channel != transmitChannel) {
+            if (this == sender || channel != transmitChannel)
+            {
                 Log.Info("I am the sender or channels are not equal:" + channel + ", " + transmitChannel);
                 return;
             }
@@ -276,8 +385,10 @@ namespace Lib
             indicateReceive(true);
         }
 
-        public void receiveThrottle(RadioControl sender, float throttle, int transmitChannel) {
-            if (this == sender || channel != transmitChannel) {
+        public void receiveThrottle(RadioControl sender, float throttle, int transmitChannel)
+        {
+            if (this == sender || channel != transmitChannel)
+            {
                 Log.Info("I am the sender or channels are not equal:" + channel + ", " + transmitChannel);
                 return;
             }
@@ -286,22 +397,28 @@ namespace Lib
             indicateReceive(true);
         }
 
-        ~RadioControl() {
+        ~RadioControl()
+        {
             Log.Info("Destructor called");
         }
 
-        public override void OnInactive() {
+        public override void OnInactive()
+        {
             Log.Info("OnInactive called");
             base.OnInactive();
         }
 
-        public override void OnStart(StartState state) {
-            if (state == StartState.Editor) {
+        public override void OnStart(StartState state)
+        {
+            if (state == StartState.Editor)
+            {
                 this.part.OnJustAboutToBeDestroyed += OnJustAboutToBeDestroyed;
             }
-            else {
+            else
+            {
                 Channel.radioListeners.Add(this);
-                foreach (var listener in Channel.radioListeners) {
+                foreach (var listener in Channel.radioListeners)
+                {
                     if (listener.vessel != null)
                         Log.Info("Listener found:" + listener.vessel.vesselName);
                 }
@@ -312,14 +429,17 @@ namespace Lib
 
 
 
-        public override void OnUpdate() {
+        public override void OnUpdate()
+        {
 
-            if (enableSync && updateCounter % 30 == 0 && this.vessel == FlightGlobals.ActiveVessel) {
+            if (enableSync && updateCounter % 30 == 0 && this.vessel == FlightGlobals.ActiveVessel)
+            {
                 transmitRotation(this.vessel.GetTransform().rotation, false);
             }
             updateCounter++;
 
-            if (fireTime != 0 && fireTime + lightOnTime <= Time.time) {
+            if (fireTime != 0 && fireTime + lightOnTime <= Time.time)
+            {
                 lightsOff();
             }
 
@@ -329,7 +449,7 @@ namespace Lib
 
         }
 
-        
+
         public void Update() //AGX: The OnUpdate above only seems to run in flight mode, Update() here runs in all scenes
         {
             if (agxGroupType == "1" & groupLastUpdate != "1" || agxGroupType != "1" & groupLastUpdate == "1") //AGX: Monitor group to see if we need to refresh window
@@ -344,12 +464,14 @@ namespace Lib
         }
 
 
-        public void OnDetach(bool first) {
+        public void OnDetach(bool first)
+        {
             Channel.radioListeners.Remove(this);
             Log.Info("OnDetach");
         }
 
-        private void OnJustAboutToBeDestroyed() {
+        private void OnJustAboutToBeDestroyed()
+        {
             Channel.radioListeners.Remove(this);
             Log.Info("OnJustAboutToBeDestroyed");
         }
