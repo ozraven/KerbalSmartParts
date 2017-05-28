a6e6a6e0dbcb76c3a4377a21c5cfe98743dc0989
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-28 11:35:46 -0400

    Updated for 1.3

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -10,4 +10,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.9.6.0")]
+ [assembly: AssemblyVersion("1.9.7.0")]
--- Source/buildRelease.bat
+++ Source/buildRelease.bat
@@ -1,29 +1,9 @@
-﻿
-rem @echo off
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
+﻿rem
+
+@echo off
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-)
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
-
-
-rem type SmartParts.version
-rem set /p VERSION= "Enter version: "
 
 
 set VERSIONFILE=SmartParts.version
@@ -45,43 +25,37 @@ if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 
 echo %VERSION%
 
-pause
+mkdir ..\GameData\SmartParts
+mkdir ..\GameData\SmartParts\Parts
+mkdir ..\GameData\SmartParts\Plugins
+mkdir ..\GameData\SmartParts\Sounds
 
 
+del /Q ..\GameData\SmartParts
+del /Q ..\GameData\SmartParts\Parts
+del /Q ..\GameData\SmartParts\Plugins
+del /Q ..\GameData\SmartParts\Sounds
 
 
-mkdir %HOMEDIR%\install\GameData\SmartParts
-mkdir %HOMEDIR%\install\GameData\SmartParts\Parts
-mkdir %HOMEDIR%\install\GameData\SmartParts\Plugins
-mkdir %HOMEDIR%\install\GameData\SmartParts\Sounds
+copy /Y "%~dp0..\bin\Release\SmartParts.dll" "..\GameData\SmartParts\Plugins"
+
+copy /Y "%~dp0SmartParts.version" "..\GameData\SmartParts"
+
+xcopy /Y /S "%~dp0..\OrigParts" "..\GameData\SmartParts\Parts"
 
 
-del /Q %HOMEDIR%\install\GameData\SmartParts
-del /Q %HOMEDIR%\install\GameData\SmartParts\Parts
-del /Q %HOMEDIR%\install\GameData\SmartParts\Plugins
-del /Q %HOMEDIR%\install\GameData\SmartParts\Sounds
+del ..\GameData\SmartParts\Parts\Fuel-Breakers\*.tga
+del ..\GameData\SmartParts\Parts\Fuel-Controller\*.tga
+del ..\GameData\SmartParts\Parts\Smart-Controller\*.tga
+del ..\GameData\SmartParts\Parts\Valve\*.tga
 
 
-copy /Y "%~dp0..\bin\Release\SmartParts.dll" "%HOMEDIR%\install\GameData\SmartParts\Plugins"
+copy /Y "%~dp0..\License.txt" "..\GameData\SmartParts"
+copy /Y "%~dp0..\README.md" "..\GameData\SmartParts"
 
-copy /Y "%~dp0SmartParts.version" "%HOMEDIR%\install\GameData\SmartParts"
+copy /Y MiniAVC.dll  "..\GameData\SmartParts"
 
-xcopy /Y /S "%~dp0..\GameData\SmartParts\Parts" "%HOMEDIR%\install\GameData\SmartParts\Parts"
-
-
-del %HOMEDIR%\install\GameData\SmartParts\Parts\Fuel-Breakers\*.tga
-del %HOMEDIR%\install\GameData\SmartParts\Parts\Fuel-Controller\*.tga
-del %HOMEDIR%\install\GameData\SmartParts\Parts\Smart-Controller\*.tga
-del %HOMEDIR%\install\GameData\SmartParts\Parts\Valve\*.tga
-
-
-copy /Y "%~dp0..\License.txt" "%HOMEDIR%\installv\GameData\SmartParts"
-copy /Y "%~dp0..\README.md" "%HOMEDIR%\install\GameData\SmartParts"
-
-copy /Y MiniAVC.dll  "%HOMEDIR%\install\GameData\SmartParts"
-
-%HOMEDRIVE%
-cd %HOMEDIR%\install
+cd ..
 
 set FILE="%RELEASEDIR%\SmartParts-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
